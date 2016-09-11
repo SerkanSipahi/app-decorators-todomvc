23218551ce3964ba5480888a8a18279ddeadf020
Serkan Sipahi <serkan.sipahi@yahoo.de>
2016-09-12 00:43:29 +0200

    Remove todo-filter; add router

--- src/app-todomvc.js
+++ src/app-todomvc.js
@@ -1,9 +1,8 @@
 
-import { component, view } from 'app-decorators';
+import { component, view, on, Router } from 'app-decorators';
 
 import './todo-new';
 import './todo-list';
-import './todo-filter';
 
 @component({
     name: 'app-todomvc',
@@ -15,18 +14,41 @@ import './todo-filter';
             <input is="todo-new" placeholder="What needs to be done?" autofocus="">
         </header>
         <section class="main">
-            <input class="toggle-all" id="toggle-all" type="checkbox" />
+            <input class="toggle-all mark-all" id="toggle-all" type="checkbox" />
             <label for="toggle-all">Mark all as complete</label>
             <ul is="todo-list"></ul>
         </section>
         <footer class="footer">
             <span class="todo-count"><strong>0</strong> items left</span>
-            <ul is="todo-filter"></ul>
+            <ul class="filters">
+                <li class="all"><a href="/todomvc/filter-all">All</a></li>
+                <li class="active"><a href="/todomvc/filter-active">Active</a></li>
+                <li class="completed"><a href="/todomvc/filter-completed">Completed</a></li>
+            </ul>
             <button class="clear-completed">Clear completed</button>
         </footer>
     </section>
 `)
 class Todomvc {
+
+    created(){
+
+        let router = Router.create({ scope: this });
+        router.on('filter /todomvc/filter-{{type}}', ::this.filter);
+    }
+
+    @on('changed .mark-all') markAll(){
+
+    }
+
+    @on('click .clear-completed') clearCompleted(){
+
+    }
+
+    filter({ params }){
+
+        console.log('filter', params.type);
+    }
 }
 
 export {

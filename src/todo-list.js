af879ccaf310b9449d17a84ab7ddc0e409f33481
Serkan Sipahi <serkan.sipahi@yahoo.de>
2016-09-17 22:25:49 +0200

    todo-list.js: refactor

--- src/todo-list.js
+++ src/todo-list.js
@@ -1,6 +1,14 @@
 
 import { component, on } from 'app-decorators';
 import { TodoItem } from './todo-item';
+import { trigger } from './trigger';
+
+import {
+    EVENT_TODO_NEW_ITEM,
+    EVENT_TODO_LIST_COUNTS,
+    EVENT_TODO_COMPLETED_ITEM,
+    EVENT_TODO_DELETEED_ITEM
+} from './todo-events';
 
 @component({
     name: 'todo-list',
@@ -8,16 +16,47 @@ import { TodoItem } from './todo-item';
 })
 class TodoList {
 
-    created(){
 
-        this.classList.add('todo-list');
+    @on(EVENT_TODO_COMPLETED_ITEM) onCompleted({ target }){
+
+        target.classList.toggle('completed');
+
+        this._updateCounts();
+
     }
 
-    @on('todo-new', document) onNewTodo( event ){
+    @on(EVENT_TODO_DELETEED_ITEM) onDeleted({ target }){
 
-        let todoItem = TodoItem.create({ text: event.detail });
+        target.parentElement.removeChild(target);
+
+        this._updateCounts();
+
+    }
+
+    @on(EVENT_TODO_NEW_ITEM, document) onNewItem({ params }){
+
+        let todoItem = TodoItem.create({ text: params });
         this.appendChild(todoItem);
+
+        this._updateCounts();
+
     }
+
+    _updateCounts(){
+
+        // shortcut for querySelectorAll
+        let $         = ::this.querySelectorAll;
+        // event args
+        let count     = $('li').length;
+        let left      = $('li:not(.completed)').length;
+        let completed = $('li.completed').length;
+
+        trigger(EVENT_TODO_LIST_COUNTS, {
+            count, left, completed
+        }, this);
+
+    }
+
 }
 
 export {
